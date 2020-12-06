e018e8f26a3f055f5ad519820144cdc2ce3d31a9
MayuraR <rajadhyaksha.mayura@gmail.comut config --global user.name MayuraRgit config --global user.email rajadhyaksha.mayura@gmail.com>
2020-12-06 16:16:51 +0530

    Angular Assignments

--- Angular/Assignment2/Assignment/src/app/app.module.ts
+++ Angular/Assignment2/Assignment/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http'
 import { FormsModule } from '@angular/forms'
 import { AppRoutingModule, routeComponent} from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -13,6 +14,7 @@ import { AdvertisementServiceService } from './advertisement-service.service';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     FormsModule
   ],
   providers: [],
